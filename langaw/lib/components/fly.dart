417d5859e00ab494fc62034f0443a62b11363f10
Pedro Constantino <pedro.constantino@ctw.bmwgroup.com>
2019-12-19 00:00:54 +0000

    flies and behaviours

--- langaw/lib/components/fly.dart
+++ langaw/lib/components/fly.dart
@@ -1,22 +1,29 @@
 import 'dart:ui';
 import 'package:flutter/cupertino.dart';
 import 'package:langaw/langaw-game.dart';
+import 'package:flame/sprite.dart';
 
 class Fly {
   Rect flyRect;
-  Paint flyPaint;
   final LangawGame game;
   bool isDead = false;
   bool isOffScreen = false;
+  List<Sprite> flyingSprite;
+  Sprite deadSprite;
+  double flyingSpriteIndex = 0;
+  double get speed => game.tileSize * 3;
+  Offset targetLocation;
 
-  Fly({this.game, double x, double y}) {
-    flyRect = Rect.fromLTWH(x, y, game.tileSize, game.tileSize);
-    flyPaint = Paint();
-    flyPaint.color = Color(0xff6ab04c);
+  Fly(this.game){
+    setTargetLocation();
   }
 
   void render(Canvas c) {
-    c.drawRect(flyRect, flyPaint);
+    if (isDead) {
+      deadSprite.renderRect(c, flyRect.inflate(2));
+    } else {
+      flyingSprite[flyingSpriteIndex.toInt()].renderRect(c, flyRect.inflate(2));
+    }
   }
 
   void update(double t) {
@@ -26,12 +33,32 @@ class Fly {
       if (flyRect.top > game.screenSize.height) {
         isOffScreen = true;
       }
+    } else {
+      flyingSpriteIndex += 30 * t;
+      if (flyingSpriteIndex >= 2) {
+        flyingSpriteIndex -= 2;
+      }
+
+      double stepDistance = speed * t;
+      Offset toTarget = targetLocation - Offset(flyRect.left, flyRect.top);
+      if (stepDistance < toTarget.distance) {
+        Offset stepToTarget = Offset.fromDirection(toTarget.direction, stepDistance);
+        flyRect = flyRect.shift(stepToTarget);
+      } else {
+        flyRect = flyRect.shift(toTarget);
+        setTargetLocation();
+      }
     }
   }
 
   void onTapDown() {
-    flyPaint.color = Color(0xffff4757);
     isDead = true;
     game.spawnFly();
+  }
+
+  void setTargetLocation() {
+    double x = game.random.nextDouble() * (game.screenSize.width - (game.tileSize * 2.025));
+    double y = game.random.nextDouble() * (game.screenSize.height - (game.tileSize * 2.025));
+    targetLocation = Offset(x, y);
   }
 }
--- langaw/lib/langaw-game.dart
+++ langaw/lib/langaw-game.dart
@@ -6,10 +6,11 @@ import 'package:flutter/material.dart';
 import 'package:langaw/components/backyard.dart';
 import 'package:langaw/components/fly.dart';
 import 'package:flutter/gestures.dart';
-import 'package:flutter/gestures.dart';
-
-
-
+import 'package:langaw/components/house-fly.dart';
+import 'package:langaw/components/agile-fly.dart';
+import 'package:langaw/components/drooler-fly.dart';
+import 'package:langaw/components/hungry-fly.dart';
+import 'package:langaw/components/macho-fly.dart';
 
 class LangawGame extends Game {
   Size screenSize;
@@ -66,8 +67,25 @@ class LangawGame extends Game {
   }
 
   void spawnFly() {
-    double x = random.nextDouble() * (screenSize.width - tileSize);
-    double y = random.nextDouble() * (screenSize.height - tileSize);
-    flies.add(Fly(game: this, x: x, y: y));
+    double x = random.nextDouble() * (screenSize.width - (tileSize * 2.025));
+    double y = random.nextDouble() * (screenSize.height - (tileSize * 2.025));
+
+    switch (random.nextInt(5)) {
+      case 0:
+        flies.add(HouseFly(this, x, y));
+        break;
+      case 1:
+        flies.add(DroolerFly(this, x, y));
+        break;
+      case 2:
+        flies.add(AgileFly(this, x, y));
+        break;
+      case 3:
+        flies.add(MachoFly(this, x, y));
+        break;
+      case 4:
+        flies.add(HungryFly(this, x, y));
+        break;
+    }
   }
 }
